7d81d07af7bd131c8b16d789dc0b565d6f6e3c47
lycoris11 <pinto9435@gmail.com>
2019-12-08 11:29:24 -0500

    Production Ready

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,29 @@
 
+//WOW.js Initialzation
 new WOW().init();
+
+//Background Color Change
+$(window).scroll(function() {
+    if ($(this).scrollTop() <= 450) {
+        $('#wrapper').addClass('colorOne').removeClass('colorTwo');
+    } else if ($(this).scrollTop() <= 800) {
+        $('#wrapper').addClass('colorTwo').removeClass('colorThree');
+    } else if ($(this).scrollTop() <= 1000) {
+        $('#wrapper').addClass('colorThree').removeClass('colorOne');
+    } else {
+        $('#wrapper').addClass('colorThree');
+    }
+});
+
+
+$(document).ready(function () {
+
+    //enable tooltips
+    options = { animation:true,}
+    $('[data-toggle="tooltip"]').tooltip(options);
+
+    //enable modals on mobile
+    $('#whoopsModal').on('shown.bs.modal', function () {
+        $('#modalInput').trigger('focus');
+    })
+});
