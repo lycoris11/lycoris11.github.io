cd2bea346181055ca2e467f3c1a3413b0cf0b1c8
lycoris11 <pinto9435@gmail.com>
2023-07-29 14:35:14 -0500

    test

--- next.config.mjs
+++ next.config.mjs
@@ -20,22 +20,3 @@ const withMDX = nextMDX({
 })
 
 export default withMDX(nextConfig)
-
-// next.config.js
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-}
