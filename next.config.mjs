32d9b9331438aeb44df1762eed83cbca8dff659c
lycoris11 <pinto9435@gmail.com>
2023-07-29 15:46:32 -0500

    errors

--- next.config.mjs
+++ next.config.mjs
@@ -11,16 +11,11 @@ const nextConfig = {
   },
 }
 
-const withMDX = require('@next/mdx')({
+const withMDX = nextMDX({
   extension: /\.mdx?$/,
   options: {
-    // If you use remark-gfm, you'll need to use next.config.mjs
-    // as the package is ESM only
-    // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [],
-    rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypePrism],
   },
 })
 
