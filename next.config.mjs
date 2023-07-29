62b88b7b3bf75c4d92e5b51d06d1421067b4eb1a
lycoris11 <pinto9435@gmail.com>
2023-07-29 15:17:34 -0500

    add loader

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
-import rehypePrism from '@mapbox/rehype-prism';
-import nextMDX from '@next/mdx';
-import remarkGfm from 'remark-gfm';
+import rehypePrism from '@mapbox/rehype-prism'
+import nextMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -18,14 +18,17 @@ const nextConfig = {
           {
             loader: "@mdx-js/loader",
             options: {
-              remarkPlugins: [],
+              remarkPlugins: [remarkGfm],
+              rehypePlugins: [rehypePrism],
             },
           },
         ],
       });
     }
+
+    return config;
   },
-};
+}
 
 const withMDX = nextMDX({
   extension: /\.mdx?$/,
@@ -33,6 +36,6 @@ const withMDX = nextMDX({
     remarkPlugins: [remarkGfm],
     rehypePlugins: [rehypePrism],
   },
-});
+})
 
-export default withMDX(nextConfig);
+export default withMDX(nextConfig)
