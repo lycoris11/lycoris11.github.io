eae19fbc00f8fa7dcb77e31fe40c84ae06a5bfcd
lycoris11 <pinto9435@gmail.com>
2023-08-01 08:46:31 -0500

    Import Cleanup

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,9 +6,7 @@ import clsx from 'clsx'
 import { Container } from '@/components/Container'
 import {
   GitHubIcon,
-  InstagramIcon,
   LinkedInIcon,
-  TwitterIcon,
 } from '@/components/SocialIcons'
 import portraitImage from '@/images/portrait.png'
 
