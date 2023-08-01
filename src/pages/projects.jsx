b1195eb7b22b99c7405c4fc692bfbcbe9bf6c489
lycoris11 <pinto9435@gmail.com>
2023-08-01 08:37:05 -0500

    Deployment Cleanup

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -49,7 +49,7 @@ export default function Projects() {
       </Head>
       <SimpleLayout
         title="Things I’ve made trying to put my dent in the universe."
-        intro="I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
+        intro="I have engaged in numerous small projects throughout the years, but I take particular pride in these ones. Most of them are open-source, meaning that if you come across something that catches your attention, feel free to explore the code and offer any suggestions for enhancement."
       >
         <ul
           role="list"
